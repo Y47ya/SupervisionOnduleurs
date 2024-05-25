1c542d958ca361b588d1fee482c8b7dd45b363b6
yahya <yahyahadir388@gmail.com>
2024-05-25 13:53:06 +0100

    final

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ from PyQt5.QtWidgets import QApplication, QMessageBox
 from src.view.window import MyWindow
 
 app = QApplication(sys.argv)
-window = MyWindow()
-sys.exit(app.exec_())
+window = None
 
-# try:
-#     window = MyWindow()
-# except ValueError as e:
-#     alert_box = QMessageBox()
-#     alert_box.setWindowTitle("Erreur")
-#     alert_box.setText("Vérifiez la connexion avec les onduleurs")
-#     alert_box.exec_()
-#     if window:
-#         window.close()
-#     sys.exit(-1)
-#
-# sys.exit(app.exec_())
+try:
+    window = MyWindow()
+except ValueError as e:
+    alert_box = QMessageBox()
+    alert_box.setWindowTitle("Erreur")
+    alert_box.setText("Vérifiez la connexion avec les onduleurs")
+    alert_box.exec_()
+    if window:
+        window.close()
+    sys.exit(-1)
+
+sys.exit(app.exec_())
